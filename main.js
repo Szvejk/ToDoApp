ca763b22fc609ec142de55f7e3f0fecdcbc3b3d7
Sara Szwejkowska <saraszw@op.pl>
2022-06-27 23:48:59 +0200

    remove and add tasks

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const toDoList = []
+
+
+
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
@@ -6,25 +10,40 @@ const input = document.querySelector('input');
 
 
 const removeTask = (e) => {
-e.target.parentNode.remove();
+const index = e.target.parentNode.dataset.key;
+toDoList.splice(index, 1)
+console.log(toDoList);
 taskNumber.textContent = listItems.length;
+renderList();
 }
 
 const addTask = (e) => {
 e.preventDefault()
 const titleTask = input.value; 
-if(titleTask === "") return;
-console.log(titleTask);
+if (titleTask === "") return;
 const task = document.createElement('li');
 task.className = 'task';
 task.innerHTML = titleTask + "<button>Usuń</button>";
+toDoList.push(task)
+renderList ()
+
+
 ul.appendChild(task);
 input.value = "";
 // const liItems = document.querySelectorAll('li.task').length;
 taskNumber.textContent = listItems.length;
 document.querySelector('li:last-child button').addEventListener('click', removeTask);
-
 }
+
+const renderList = () => {
+    ul.textContent = "";
+toDoList.forEach((toDoElement, key ) => {
+    toDoElement.dataset.key = key;
+ul.appendChild(toDoElement);
+}
+)
+}
+
 form.addEventListener('submit', addTask)
 
 
