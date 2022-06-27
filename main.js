bf335d0d0a4731e5c03b6dd11e0e55d5319b77b7
Sara Szwejkowska <saraszw@op.pl>
2022-06-27 23:14:59 +0200

    created addTask

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 const form = document.querySelector('form');
-const li = document.querySelector('ul');
-const taskNumber = document.querySelector('h1 span');
-const listItems = document.querySelector('form');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const input = document.querySelector('input');
 const ul = document.querySelector('ul');
-const liElements = document.querySelectorAll('li');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector('input');
+
+const addTask = (e) => {
+e.preventDefault()
+const titleTask = input.value; 
+if(titleTask === "") return;
+console.log(titleTask);
+const task = document.createElement('li');
+task.className = 'task';
+task.innerHTML = titleTask + "<button>Usuń</button>";
+ul.appendChild(task);
+input.value = "";
+// const liItems = document.querySelectorAll('li.task').length;
+taskNumber.textContent = listItems.length;
+}
+form.addEventListener('submit', addTask)
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('input');
+// const ul = document.querySelector('ul');
+// const liElements = document.querySelectorAll('li');
 
 const searchTask = (e) => {
     const searchText = (e.target.value.toLowerCase())
